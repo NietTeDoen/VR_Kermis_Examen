4beadb2606a050f7c160e3cb8499238ea30ddddc
NietTeDoen <hfhaandrikman@gmail.com>
2022-11-05 15:23:13 +0100

    update V0.7
    reworked the way of counting points, removed the glitch of a can explosion. and you won't get all the points anymore if you miss the ball.

--- VR Kermis Examen/Assets/ResetScript.cs
+++ VR Kermis Examen/Assets/ResetScript.cs
@@ -3,32 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class ResetScript : MonoBehaviour
 {
-    public GameObject CanPrefab;
+    public static bool GameObjectCount = false;
+    public List<GameObject> CansList = new List<GameObject>();
+    public GameObject[] CansAmmount = new GameObject[2];
+    public GameObject prefabCans;
     public GameObject Cans;
+    public GameObject ball;
+    public GameObject prefabBall;
     public Text ScoreTXT;
-    // Start is called before the first frame update
+
     void Start()
-    {
-        ScoreTXT.text = score_system.score.ToString();
-    }
+    {ScoreTXT.text = score_system.score.ToString();}
 
-    // Update is called once per frame
     void Update()
-    {
-        
-        ScoreTXT.text = score_system.score.ToString();
+    {ScoreTXT.text = score_system.score.ToString(); 
     }
-
-    public void OnTriggerEnter(Collider other)
+    public IEnumerator CanReset()
     {
-        if (other.tag == "Ground")
+        ScoreTXT.text = score_system.score.ToString(); GameObject.Destroy(Cans, 1.5f); yield return new WaitForSeconds(2f); GameObject.Destroy(ball); CansList.RemoveRange(0, CansList.Count);
+        Cans = Instantiate(prefabCans); ball = Instantiate(prefabBall);
+        GameObjectCount = false;
+        yield return null; StopCoroutine("CanReset");
+    }
+    public void coroutineStarter(){
+        if (!GameObjectCount) {
+            GameObjectCount = true;
+            foreach (GameObject CanGo in GameObject.FindGameObjectsWithTag("Cans"))
+            {
+                CansList.Add(CanGo);
+                print(CansList.Count);
+            }
+        }
+        
+        
+        if (CansList.Count > 15)
         {
-            GameObject.Destroy(Cans, 0.75f);
-            Cans = Instantiate(CanPrefab);
-            ScoreTXT.text = score_system.score.ToString();
+            foreach (GameObject go in GameObject.FindGameObjectsWithTag("Cans"))
+            {
+                Destroy(go ,0.01f);
+                print("error: there were more cans then 15 in the scene... removing cans");
+            }
+            CansList.RemoveRange(0, CansList.Count);
+            StartCoroutine("CanReset");
+
+        }
+        else
+        {
+            StartCoroutine("CanReset");
         }
     }
 }
--- VR Kermis Examen/Assets/score_system.cs
+++ VR Kermis Examen/Assets/score_system.cs
@@ -5,34 +5,19 @@ using UnityEngine.UI;
 
 public class score_system : MonoBehaviour
 {
-
-    
+    public GameObject CanHandler;
+    public ResetScript RS;
     public static int score;
     private bool HasFallen = false;
-    public Text ScoreTXT;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //ScoreTXT.text = score.ToString();
-    }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void Start()
     {
-
+        CanHandler = GameObject.FindGameObjectWithTag("CanScript");
+        RS = CanHandler.GetComponent<ResetScript>();
     }
 
     public void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Ground" & !HasFallen)
-        {
-            score += 1;
-            HasFallen = true;
-            
-
-
-        }
-        
-    }
-
+    {if (other.tag == "Ground" & !HasFallen)
+        { score += 1; HasFallen = true; 
+            RS.coroutineStarter();}}
 }
